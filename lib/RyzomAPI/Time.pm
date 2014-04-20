37611fb288324c3b170f101183fe00210129acd5
cahu <cahu@gmx.us>
2014-04-20 17:03:09 +0200

    Time submodule skeleton

---
+++ lib/RyzomAPI/Time.pm
@@ -0,0 +1,36 @@
+use v5.014;
+use strict;
+use warnings;
+
+use Mouse;
+
+
+has [
+	'server_tick',
+	'jena_year',
+	'day_of_jy',
+	'month_of_jy',
+	'cycle',
+	'day_of_cycle',
+	'month_of_cycle',
+	'day_of_month',
+	'day_of_week',
+	'season',
+	'day_of_season',
+	'time_of_day',
+] => (
+	is  => 'rw',
+	isa => 'Int',
+);
+
+has [
+	'txt_en',
+	'txt_fr',
+	'txt_de',
+] => (
+	is  => 'rw',
+	isa => 'Str',
+);
+
+
+1;
